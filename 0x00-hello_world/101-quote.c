f390f8f585b6b8d6bf2b0b05a86ad0e1e9bdc0b2
kenkeluke <halimahogunbiyi@gmail.com>
2022-06-03 04:32:32 +0000

    Understanding simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Description: this program prints quote to the standard error
@@ -9,7 +9,7 @@
 int main(void)
 {
 
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 
 }
