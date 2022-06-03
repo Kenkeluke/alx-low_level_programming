9580de183e30ff048d946d07545cf724ad6db6ba
kenkeluke <halimahogunbiyi@gmail.com>
2022-06-03 01:11:40 +0000

    Hello, printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: this program prints a string
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+puts(\"Programming is like building a multilingual puzzle)
+return (0);
+
+}
