ed8087a6a29e9fdfc7ff8b61d37d1633105c7b1b
kenkeluke <halimahogunbiyi@gmail.com>
2022-06-09 23:08:39 +0000

    prints all digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_numbers - entry point
+ *Description :This function prints numbers 0 - 9
+ *
+ *@void : no return value
+ *Return: void;
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+int i = 48;
+while (i < 58)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+
+}
