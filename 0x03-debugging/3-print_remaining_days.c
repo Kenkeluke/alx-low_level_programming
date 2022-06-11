c961e89bada44db103bb659d7cd420a918768e2b
kenkeluke <halimahogunbiyi@gmail.com>
2022-06-11 01:16:02 +0000

    Remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,7 +16,7 @@ if ((year % 100 == 0  && year % 400 == 0) || (year % 4 == 0))
 {
 if (month >= 2 && day < 60)
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 }
 
 printf("Day of the year: %d\n", day);
@@ -26,7 +26,7 @@ else
 {
 if (month == 2 && day == 60)
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 }
 else
 {
