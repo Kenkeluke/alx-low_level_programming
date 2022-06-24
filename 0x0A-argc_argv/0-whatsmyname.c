9157806445087e11ce89cffde88aab791293d03e
kenkeluke <halimahogunbiyi@gmail.com>
2022-06-24 23:50:21 +0000

    oh na na What's my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description : This program that prints its name
+ * followed by a new line
+ *@argc: command line argument count
+ *@argv: array of command line string arguments
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc >= 0)
+{
+printf("%s\n", argv[0]);
+
+}
+
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/**
+ * This is a function declaration.
+ * It tells the compiler that the function * exists somewhere.
+ */
+
+int _putchar(char c);
+
+
+
+
+
+#endif /* MAIN_H */
