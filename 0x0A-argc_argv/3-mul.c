8b6d0fa00507442dd890fee01e28ed4d91150bbb
kenkeluke <halimahogunbiyi@gmail.com>
2022-06-25 04:43:47 +0000

    adding files

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: this funtion multiplies two number arg * uments passed to the
+ *command line
+ * arguments passed to the command line
+ * followed by a new line
+ * @argc: number of arguments
+ * @argv: array of arguments type into the
+ * command line
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+if (argc != 3)
+{
+printf("Error\n");
+
+}
+
+i = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", i);
+return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+ /**
+ *main - entry point
+ *Description :The command line followed by a new line
+ * @argc: number of arguments
+ * @argv: array of arguments type into the
+ * command line
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+int i, j, sum = 0;
+for (i = 1; i < argc; i++)
+{
+
+
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if ((argv[i][j] < '0' || argv[i][j] > '9'))
+{
+printf("Error\n");
+return (1);
+}
+
+}
+sum += atoi(argv[i]);
+
+}
+
+printf("%d\n", sum);
+return (0);
