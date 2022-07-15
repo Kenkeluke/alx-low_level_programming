2f5678a24a6c38159b0484ef9c54045f43005bd4
kenkeluke <halimahogunbiyi@gmail.com>
2022-07-14 21:56:15 -0700

    first version of add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,8 +8,8 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t **result;
-list_t **new_node = malloc(sizeof(size_t));
+list_t *result;
+list_t *new_node = malloc(sizeof(size_t));
 new_node -> str = strdup(str);
 new_node -> next = NULL;
 new_node -> next = head;
