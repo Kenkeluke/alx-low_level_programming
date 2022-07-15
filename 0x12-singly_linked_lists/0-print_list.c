fe6a4383f6f50eb5e9cda747e3413f172384481e
kenkeluke <halimahogunbiyi@gmail.com>
2022-07-15 03:54:19 +0000

    fixing betty specificstions

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,32 +1,29 @@
 #include "lists.h"
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * print_list - print elements of a single linked list
+ * @h: pointer to list param
+ * Return: number of nodes
  *
  * Description: singly linked list node structure
  */
 
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
-   const list_t *node = h;
-    while (node != NULL)
-    {
-
-        if (node->str == NULL)
-        {
-            printf("[0] (nil)\n");
-        }
-        else {
-            printf("[%u]", node->len);
-            printf("%s\n", node->str);
-
-        }
-
-        node = node->next;
-        count++;
-    }
-    return (count);
+size_t count = 0;
+const list_t *node = h;
+while (node != NULL)
+{
+if (node->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%u]", node->len);
+printf("%s\n", node->str);
+}
+node = node->next;
+count++;
+}
+return (count);
 }
