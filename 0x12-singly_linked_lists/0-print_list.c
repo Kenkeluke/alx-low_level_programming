520a145ea86bf1e65e889b7559372f729698c4ac
kenkeluke <halimahogunbiyi@gmail.com>
2022-07-14 20:11:46 -0700

    print list function

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+size_t print_list(const list_t *h)
+{
+    size_t count = 0;
+   const list_t *node = h;
+    if (node->str == NULL)
+        printf("[0] (nil)");
+    while (node->next != NULL)
+    {
+        printf("[%ui] %s", node->len, node->str);
+        node = node->next;
+        count++;
+    }
+    return (count);
+}
