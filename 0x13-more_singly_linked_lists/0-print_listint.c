bc836d8709b75591d1babe34d463c4521a00f48f
kenkeluke <halimahogunbiyi@gmail.com>
2022-07-19 03:17:38 -0700

    print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints elements of a list
+ * Description: This function uses a for loop
+ * to print the elements of a list
+ * @h: pointer to the head of the list
+ * 
+ * Return: an unsigned integer, the number of
+ * nodes in the list.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+size_t num_nodes = 0;
+const listint_t* node = (listint_t *)malloc(sizeof(listint_t));
+node = h;
+
+/*bringing in the loop*/
+while (node != NULL)
+{
+num_nodes++;        
+node =  node->next;
+}
+return(num_nodes);
+
+}
