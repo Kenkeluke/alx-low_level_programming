480ee8404a0934b8a7f653059afc4f3765a80421
kenkeluke <halimahogunbiyi@gmail.com>
2022-07-19 02:44:32 -0700

    adding necessary files

---
+++ 0x13-more_singly_linked_lists/.history/lists_20220719022936.h
@@ -0,0 +1,30 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+
+
+#endif /* LISTS_H */
