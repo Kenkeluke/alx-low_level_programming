6cfe36e8e66d8fdd48a57c49539dbdd40409d387
kenkeluke <halimahogunbiyi@gmail.com>
2022-07-19 19:02:37 -0700

    add node function

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *listint_len - function that returns the number of elements in a list
+ *@h: pointer to the head
+ *
+ *Description: counts the number of elements in a singly linked list
+ *Return: number of elements in the list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+size_t num_elements;
+num_elements = 0;
+while(h->next !=NULL)
+{
+num_elements++;
+}
+return (num_elements);
+}
