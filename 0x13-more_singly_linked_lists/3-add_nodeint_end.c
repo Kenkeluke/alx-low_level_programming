a009150523c1ef7bd907fc93159342db5067a853
Kenkeluke <halimahogunbiyi@gmail.com>
2022-07-21 02:07:56 -0700

    newer version of add_nodeint_end function

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,20 +14,21 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *new_node = malloc(sizeof(listint_t));
-listint_t *last_node = malloc(sizeof(listint_t));
-last_node = *head;
+listint_t *last_node;
+
 new_node->n = n;
 new_node->next = NULL;
-if (head == NULL)
+if (*head == NULL)
 {
 *head = new_node;
-return (*head);
+return (new_node);
 }
+last_node = *head;
 while (last_node->next != NULL)
 {
 last_node = last_node->next;
 }
 last_node->next = new_node;
 
-return (last_node);
+return (new_node);
 }
