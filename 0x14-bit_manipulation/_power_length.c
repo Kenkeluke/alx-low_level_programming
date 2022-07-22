bb3fdf170b7e2920ee41d3bc397293f6154a6920
kenkeluke <halimahogunbiyi@gmail.com>
2022-07-21 17:30:53 -0700

    fixing betty specification

--- 0x14-bit_manipulation/_power_length.c
+++ 0x14-bit_manipulation/_power_length.c
@@ -1,53 +1,42 @@
 #include "main.h"
 
 /**
- * 
- * power- this function mimics the standard c pow()
- * Description: This function raises a given base
- * to the power of an exponent
- * @a: base parameter
- * @b: parameter exponent
- * Return: integer 
+ *power- this function mimics the standard c pow()
+ *Description: This function raises a given base
+ *to the power of an exponent
+ *@a: base parameter
+ *@b: parameter exponent
+ *Return: integer
  */
 
 int power(int a, int b)
 {
-    int result = 1;
-    while(b>0)
-    {
-        result = result * a;
-        b--;
+int result = 1;
+while (b > 0)
+{
+result = result *a;
+b--;
 
-    }
-    return (result);
+}
+return (result);
 }
 
 /**
- * 
- * Length- this function mimics the standard c string_lens
- * Description: This function counts the number
- * of character in a string
- * @s: String to counted
- * Return: integer 
+ *length- this function mimics the standard c string_lens
+ *Description: This function counts the number
+ *of character in a string
+ *@s: String to counted
+ *Return: integer
  */
 
 int length(const char *s)
 {
-    int result = 0;
-    while(*s !='\0')
-    {
-       result++;
-       s = s + 1;
-
-    }
-    return (result);
-}
-
-
-/**void main()
+int result = 0;
+while (*s != '\0')
 {
-    
-    int n = power(2, 0);
-    printf("%d", n);
-    
-}*/
+result++;
+s = s + 1;
+
+}
+return (result);
+}
